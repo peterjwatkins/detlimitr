61e47efdd6a156986b74fafc5ba03e712cd6499d
Peter Watkins <Peter.Watkins@csiro.au>
2020-08-03 15:45:10 +1000

    Modified Rmd, data.R and tests

--- R/data.R
+++ R/data.R
@@ -1,16 +1,16 @@
-#' Calibration data.
+#' Calibration data from Table 6, Vogelsang et al (1998).
 #'
-#' A dataset containing concentration (microgram per kg) and measured response (area).
+#' A sample dataset containing concentration (microgram per kg) and measured response (area).
 #'
 #' @format A data frame with 12 rows and 2 variables:
 #' \describe{
 #'   \item{concentration}{microgram/kg}
 #'   \item{area}{arbitrary units}
-#'   ...
 #' }
+#' @usage data(vhtab6)
+#' @source {J. Vogelgesang and J. Hädrich (1998), Accred. Qual. Assur., 3:242-255.}
 "vhtab6"
-
-#' Calibration data for methyl tert-butyl ether in water.
+#' Calibration dataset for methyl tert-butyl ether in water using GC-MS.
 #'
 #' A dataset containing concentration (microgram per litre) and measured response (area).
 #'
@@ -18,18 +18,19 @@
 #' \describe{
 #'   \item{concentration}{microgram/L}
 #'   \item{area}{arbitrary units}
-#'   ...
 #' }
+#' @usage data(mtbe)
+#' @source {D.T. O'Neill, E.A. Rochette and P.J. Ramsay, (2002), Anal. Chem., 74:5907-5911.}
 "mtbe"
-
-#' Calibration data for paraquat using electroanalysis.
+#' Paraquat calibration data using multiple square wave voltammetry.
 #'
 #' A dataset containing concentration (micromole per litre) and measured response (microamp X -1).
 #'
 #' @format A data frame with 9 rows and 2 variables:
 #' \describe{
 #'   \item{concentration}{micromole/L}
-#'   \item{area}{microamps X -1}
-#'   ...
+#'   \item{area}{microamps (X -1)}
 #' }
+#' @usage data(paraquat)
+#' @source {O.B. da Silva and S.A.S. Machado, (2012), Anal. Methods, 4:2348-2354}
 "paraquat"
