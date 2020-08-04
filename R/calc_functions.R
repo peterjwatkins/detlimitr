5ccec50845891b6fd1edebd25772fd2cbf23c72c
Peter Watkins <Peter.Watkins@csiro.au>
2020-08-05 09:28:12 +1000

    Tidy up of references for calc_function.R

--- R/calc_functions.R
+++ R/calc_functions.R
@@ -38,11 +38,12 @@ dl_vogelhad <- function(x, y) {
         ls[2] + s_y(x, y) * qt(0.95, n - 2) *
         sqrt(1 + 1 / n + mean(x) ^ 2 / sum((x - mean(x)) ^ 2))
     x_crit <- (y_crit - ls[2]) / ls[1]
-#   x_id <- 2 * x_crit
-#   VHdl <- as.numeric(c(xc = x_crit, xd = x_id))
     VHdl <- as.numeric(x_crit)
     return(VHdl)
 }
+#   Extra code if needed
+#   x_id <- 2 * x_crit
+#   VHdl <- as.numeric(c(xc = x_crit, xd = x_id))
 
 dl_miller <- function(x, y) {
 # Used internally to estimate the detection limit acccording to
@@ -51,12 +52,13 @@ dl_miller <- function(x, y) {
     ls <- least_sq_est(x, y)
 
     dl_calc <- (3 * s_y(x, y)) / ls[1]
-#    dl_blank <- (3 * s_y(x, y) + ls[2]) / ls[1]
-#    MMdl <- as.numeric(c(dl_c = dl_calc,
-#              dl_b = dl_blank))
     MMdl <- as.numeric(dl_calc)
     return(MMdl)
 }
+# Extra code if needed
+#    dl_blank <- (3 * s_y(x, y) + ls[2]) / ls[1]
+#    MMdl <- as.numeric(c(dl_c = dl_calc,
+#              dl_b = dl_blank))
 
 dl_hubertvos <- function(x, y, alpha = NULL, beta = NULL) {
 # Used internally to estimate the detection limit acccording to
@@ -65,7 +67,7 @@ dl_hubertvos <- function(x, y, alpha = NULL, beta = NULL) {
     beta <- ifelse(is.null(beta), 0.05, beta)
     n <- length(x)
     x.mean <- mean(x)
-    HVdl <- dl_vogelhad(x, y)[1]
+    HVdl <- dl_vogelhad(x, y)
 
     repeat {
         # Update dl
@@ -87,14 +89,21 @@ dl_hubertvos <- function(x, y, alpha = NULL, beta = NULL) {
 }
 
 #' Summarises detection limits
-#' @description A generic function summarising the detection limits estimated using three approaches;
+#' @description A generic function summarising the detection limits (DLs) estimated using three approaches;
 #' i) Miller and Miller, ii) Vogelgesang and Hädrich, and iii) Hubert & Vos.
-#' The first two are directly estimated using internal functions, \emph{dl_miller} and \emph{dl_vogelhad}
-#' while the third is iteratively estimated using \emph{dl_hubertvos}.
+#' The first two DLs are directly estimated using internal functions, \emph{dl_miller} and \emph{dl_vogelhad}
+#' while the third DL is estimated iteratively using \emph{dl_hubertvos}.
 #' By default, a single decimal point is shown but can be changed by the user.
 #' @param d A tibble containing x (concentration) and y (response).
 #' @param dp Number of decimal points
 #' @usage summaryDL(d, dp = 1)
+#' @source {
+#' i) J.C. Miller and J.N. Miller (1993), "Statistics for Analytical Chemistry",
+#' 3rd ed., Prentice-Hall.
+#' ii) J. Vogelgesang and J. Hädrich (1998), Accred. Qual. Assur., 3:242-255.
+#' iii) A. Hubaux and G. Vos (1970), Anal. Chem., 42:849-855
+#' & D.T. O'Neill, E.A. Rochette and P.J. Ramsay, (2002), Anal. Chem., 74:5907-5911
+#' }
 #' @examples
 #' data(mtbe)
 #' summaryDL(mtbe) 	    #single decimal point
