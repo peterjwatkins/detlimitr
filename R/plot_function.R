ca70063ce9deb7ae6183a90e2c6d007571ee92b3
peterjwatkins <37567097+peterjwatkins@users.noreply.github.com>
2023-03-15 14:58:03 +1100

    Revised plot function for diff regression type

--- R/plot_function.R
+++ R/plot_function.R
@@ -1,6 +1,166 @@
-##' Plots segment of calibration curve showing the estimated limits.
+base_resid_plot <- function(d, tit) {
+  ggplot2::ggplot(d,  ggplot2::aes(x, y)) +
+    ggplot2::geom_point() +
+    ggplot2::xlab("Fitted") +
+    ggplot2::ylab("Residuals") +
+    ggplot2::labs(title = tit)
+}
+
+resid_plot <- function(d , model_type = NULL) {
+  d <- adjustdf(d)
+  x <- d$x
+  y <- d$y
+  model_type <- ifelse(is.null(model_type), "l", model_type)
+
+  if (!is.character(model_type))
+    message("Model type is not known - ('l')inear/('q')uadratic/('p')ower")
+  else {
+    model_type <- set_model(model_type)
+    switch(
+      model_type,
+      "l" = {
+        model = lm(y ~ x)
+        dt <-
+          as.data.frame(cbind(x = fitted(model), y = resid(model)))
+        base_resid_plot(dt, c("Linear"))
+      },
+      "q" = {
+        model = lm(y ~ x + I(x ^ 2))
+        dt <-
+          as.data.frame(cbind(x = fitted(model), y = resid(model)))
+        base_resid_plot(dt, c("Quadratic"))
+      },
+      "p" = {
+        par <- sspwr(d$x, d$y)
+        model <-
+          nls(y ~ C + A * x ^ b, # data=d,
+              start = list(C = par[1], A = par[2], b = par[3]))
+        dt <-
+          as.data.frame(cbind(x = fitted(model), y = resid(model)))
+        base_resid_plot(dt, c("Power"))
+      },
+      message("Check model type: ('l')inear/('q')uadratic/('p')ower")
+    )
+  }
+}
+#------------------------------------------------
+baseDLplot <- function(d, x_dl, y_dl, tit) {
+  p <-  ggplot2::ggplot(d,  ggplot2::aes(x, y)) +
+    ggplot2::geom_point() +
+    ggplot2::geom_line(( ggplot2::aes(x, y = y_fit, col = "blue"))) +
+    ggplot2::geom_line(( ggplot2::aes(x, y = y_lwr, col = "red"))) +
+    ggplot2::geom_line(( ggplot2::aes(x, y = y_upr, col = "red"))) +
+    ggplot2::theme(legend.position = "none") +
+    ggplot2::xlab("Concentration") +
+    ggplot2::ylab("Response (AU)") +
+    ggplot2::labs(title = tit) +
+    ggplot2::geom_segment(
+      ggplot2::aes(
+        x = 0,
+        y = y_dl,
+        xend = x_dl,
+        yend = y_dl
+      ),
+      colour = "darkgreen",
+      linetype = "dashed"
+    )  +
+    ggplot2::geom_segment(
+      ggplot2::aes(
+        x = x_dl,
+        y = y_dl,
+        xend = x_dl,
+        yend = 0
+      ),
+      colour = "darkgreen",
+      linetype = "dashed"
+    ) + ggplot2::annotate(
+      "text",
+      x = x_dl,
+      y = 0,
+      parse = TRUE,
+      label = as.character(expression(italic("x")[HV]))
+    )
+  return(p)
+}
+
+plotlinDL <- function(x, y) {
+  d.lm <- lm(y ~ x)
+  pred_model <-
+    predict(d.lm, newdata = as.data.frame(x), interval = "prediction")
+
+  dt <-
+    data.frame(cbind(
+      x,
+      y,
+      y_fit = pred_model[, 1],
+      y_lwr = pred_model[, 2],
+      y_upr = pred_model[, 3]
+    ))
+  x_dl <- dl_hubertvos(x, y)[1]
+  y_dl <- coefficients(d.lm)[1] + coefficients(d.lm)[2] * x_dl
+
+  p <- baseDLplot(dt, x_dl, y_dl, c("Linear"))
+
+  return(p)
+}
+
+plotqDL <- function(x, y) {
+  model <- lm(y ~ x + I(x ^ 2))
+  pred_model <-
+    predict(model, newdata = as.data.frame(x), interval = "prediction")
+
+  dt <-
+    data.frame(cbind(
+      x,
+      y,
+      y_fit = pred_model[, 1],
+      y_lwr = pred_model[, 2],
+      y_upr = pred_model[, 3]
+    ))
+
+  x_dl <- dl_quad(x, y)
+  y_dl <- quad_beta(x, pred_model[, 3])[1, 1]
+
+  p <- baseDLplot(dt, x_dl, y_dl, c("Quadratic"))
+  return(p)
+}
+
+plotpowerDL <- function(x, y) {
+  dt <- calc_power(x, y)
+  co <- upr_lwr_pwr_coefs(dt)
+
+  x_dl <- dl_power(x, y)
+  y_dl <- co[1, 2]
+
+  p <- baseDLplot(dt, x_dl, y_dl, c("Power"))
+  return(p)
+}
+#------------------------------------------------------------------------------
+plotDL <- function(d, model_type = NULL) {
+  d <- adjustdf(d)
+  model_type <- ifelse(is.null(model_type), "l", model_type)
+
+  if (!is.character(model_type))
+    message("Model type is not known - ('l')inear/('q')uadratic/('p')ower")
+  else {
+    model_type <- set_model(model_type)
+    switch(
+      model_type,
+      "l" = plotlinDL(d$x, d$y),
+      "q" = plotqDL(d$x, d$y),
+      "p" = plotpowerDL(d$x, d$y),
+      message("Check model type: ('l')inear/('q')uadratic/('p')ower")
+    )
+  }
+}
+
+
+
+
+
+##' Plots segment of calibration curve showing the estimated limit for linear regression.
 #' @param d A tibble containing x (concentration) and y (response)
-#' @usage plotDL(d)
+#' @usage plotlinDL(d)
 #' @examples
 #' data(mtbe)
 #' plotDL(mtbe)
